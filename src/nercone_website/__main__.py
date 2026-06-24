78eb60a8521db9c30204d524196af35cd74a71c0
nercone-dev <nercone@nercone.dev>
2026-06-25 07:54:06 +0900

    --

--- src/nercone_website/__main__.py
+++ src/nercone_website/__main__.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import uvicorn
+from uvicorn.supervisors import Multiprocess
 
 from .databases import MimeTypes
 from .constants import Ports
@@ -13,16 +14,23 @@ def main():
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcp.bind(("127.0.0.1" if Ports.uds else "0.0.0.0", Ports.tcp))
+    tcp.set_inheritable(True)
     sockets.append(tcp)
 
     if Ports.uds:
+        try:
+            os.unlink(Ports.uds)
+        except FileNotFoundError:
+            pass
         os.umask(0o000)
-        os.unlink(Ports.uds)
         uds = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         uds.bind(Ports.uds)
+        uds.set_inheritable(True)
         sockets.append(uds)
 
-    uvicorn.run("nercone_website.app:app", sockets=sockets, workers=4, server_header=False)
+    config = uvicorn.Config("nercone_website.app:app", workers=4, server_header=False)
+    server = uvicorn.Server(config)
+    Multiprocess(config, target=server.run, sockets=sockets).run()
 
 if __name__ == "__main__":
     main()
